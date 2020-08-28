ef163ba0a8973efad6eb675981182df56351dd0c
Ram Dettmer <ramdettmer751@gmail.com>
2020-08-28 02:40:31 -0700

    updated site

---
+++ static/js/main.js
@@ -0,0 +1,54 @@
+// SLIDE HOME
+jQuery(document).ready(function ($) {
+    $('.home-slide').slick({
+  dots: false,
+  prevArrow:"<button type='button' class='slick-prev pull-left'>	&larr;	</button>",
+  nextArrow:"<button type='button' class='slick-next pull-right'>&rarr;	</button>",
+
+      pauseOnHover: false,
+      infinite: true,
+      speed: 500,
+      fade: !0,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      //slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      draggable: true,
+      arrows: true,
+      responsive: [
+      {
+      breakpoint: 1024,
+      settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      
+                }
+      },
+      {
+      breakpoint: 768,
+      settings: {
+      draggable: true,
+                }
+      },
+      {
+      breakpoint: 600,
+      settings: {
+      slidesToShow: 1,
+      draggable: true,
+      slidesToScroll: 1
+                }
+      },
+      {
+      breakpoint: 480,
+      settings: {
+      slidesToShow: 1,
+      draggable: true,
+      slidesToScroll: 1
+                }
+      }
+  
+                ]
+                });
+      });
